b7e40cd9642c04128bbe12575598516ce4ad65a4
sevoro003 <59376314+sevoro003@users.noreply.github.com>
2020-08-24 14:52:25 -0600

    Create scripts.js

---
+++ assets/scripts.js
@@ -0,0 +1,12 @@
+var popup = getElementById("popup")
+
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+    get: function(){
+        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+    }
+})
+
+if(document.querySelector('video').playing){ // checks if element is playing right now
+    // Do anything you want to
+    popup.style.display = "none";
+}
